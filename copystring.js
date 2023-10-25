4441ad6b3ab33dd40d976540fe5f19a7878c904d
Stan <stan@2meu.me>
2023-10-25 12:26:12 +0900

    initial commit

---
+++ copystring.js
@@ -0,0 +1,152 @@
+exports.copystring = function (inputOptions) {
+  const { execSync } = require("child_process");
+  const fs = require("fs");
+  const path = require("path");
+  const findup = require("findup-sync");
+
+  ///////////////////////////////////////////////////////////
+  // options
+
+  const options = inputOptions.copystring();
+  const { searchThrough, searchFor } = options;
+
+  ///////////////////////////////////////////////////////////
+  // code
+
+  function get_PATH_TO_STRING() {
+    const filePath = searchThrough.reduce((prev, cur) => {
+      return prev ?? findup(cur);
+    }, null);
+    if (!filePath) {
+      console.error(
+        "다음 파일 중 하나는 있어야 합니다 : ",
+        searchThrough.join(", ")
+      );
+      process.exit(1);
+    }
+    const targetLine = `${searchFor}=`;
+
+    const data = fs.readFileSync(filePath, "utf8");
+    const lines = data
+      .split("\n")
+      .map((line) => line.trim())
+      .filter((line) => line.length > 0)
+      .filter((line) => !line.startsWith("#"));
+
+    const found = lines.find((line) => line.startsWith(targetLine));
+
+    if (found) {
+      // Extract the path from the line
+      const PATH_TO_STRING = found.split("=")[1].trim();
+      return PATH_TO_STRING;
+    }
+    throw new Error(
+      `다음 파일[${searchThrough.join(
+        ", "
+      )}] 중 하나에 ${searchFor}를 정의하세요.
+    예시) (따옴표없음)\n  솔루션 ${searchFor}=/Users/2meu36/work/localize2dub/interviewSolution\n  클래스 ${searchFor}=/Users/2meu36/work/localize2dub/ClassWeb`
+    );
+  }
+
+  const PATH_TO_STRING = get_PATH_TO_STRING();
+  const PATH_TO_WORKING_DIR = ".";
+
+  if (!fs.existsSync(PATH_TO_STRING)) {
+    throw new Error(`스트링 폴더 찾을 수 없음 : ${PATH_TO_STRING}`);
+  }
+
+  function removeTrailingSlash(dirPath) {
+    return dirPath.replace(/\/+$/, "");
+  }
+
+  // Remove trailing slashes from PATH_TO_STRING and PATH_TO_WORKING_DIR
+  const cleanPathToWorkingDir = removeTrailingSlash(PATH_TO_WORKING_DIR);
+  const localizeDirPath = removeTrailingSlash(PATH_TO_STRING);
+
+  console.log(
+    "================================[ COPY STRING START ]================================"
+  );
+  console.log(`>> PATH_TO_STRING: ${localizeDirPath}`);
+
+  async function theFunction() {
+    // [1] git pull $LOCALIZE_DIR in $PATH_TO_STRING/$LOCALIZE_DIR
+    console.log(`>> [1] 스트링 git pull > ${localizeDirPath}`);
+    execSync(`cd ${localizeDirPath} && git pull`);
+    process.chdir(cleanPathToWorkingDir); // Return to the original working directory
+
+    // [2] copy [$PATH_TO_STRING/$LOCALIZE_DIR/$APP_NAME/en.json] to  [$PATH_TO_WORKING_DIR/public/locales/en/common.json]
+    const sourceEnJson = path.join(localizeDirPath, "en.json");
+    const destEnJson = path.join(
+      cleanPathToWorkingDir,
+      "public/locales/en/common.json"
+    );
+    const copyEn = fs.existsSync(sourceEnJson);
+    if (copyEn) {
+      if (
+        !fs.existsSync(path.join(cleanPathToWorkingDir, "public/locales/en"))
+      ) {
+        fs.mkdirSync(path.join(cleanPathToWorkingDir, "public/locales/en"), {
+          recursive: true,
+        });
+      }
+      console.log(
+        `>> [2] 영어 업데이트 완료  [${sourceEnJson}] -> [${destEnJson}]`
+      );
+      fs.copyFileSync(sourceEnJson, destEnJson);
+    } else {
+      console.log(`>> [2] 영어 스킵. 파일 없음 : [${sourceEnJson}]`);
+    }
+
+    // [3] copy [$PATH_TO_STRING/$LOCALIZE_DIR/$APP_NAME/ko.json] to  [$PATH_TO_WORKING_DIR/public/locales/ko/common.json]
+    const sourceKoJson = path.join(localizeDirPath, "ko.json");
+    const destKoJson = path.join(
+      cleanPathToWorkingDir,
+      "public/locales/ko/common.json"
+    );
+    const copyKo = fs.existsSync(sourceKoJson);
+    if (copyKo) {
+      if (
+        !fs.existsSync(path.join(cleanPathToWorkingDir, "public/locales/ko"))
+      ) {
+        fs.mkdirSync(path.join(cleanPathToWorkingDir, "public/locales/ko"), {
+          recursive: true,
+        });
+      }
+      console.log(
+        `>> [3] 한글 업데이트 완료  [${sourceKoJson}] -> [${destKoJson}]`
+      );
+      fs.copyFileSync(sourceKoJson, destKoJson);
+    } else {
+      console.log(`>> [3] 한글 스킵. 파일 없음 : [${sourceKoJson}]`);
+    }
+
+    // [4] copy [$PATH_TO_STRING/$LOCALIZE_DIR/$APP_NAME/ja.json] to  [$PATH_TO_WORKING_DIR/public/locales/ja/common.json]
+    const sourceJaJson = path.join(localizeDirPath, "ja.json");
+    const destJaJson = path.join(
+      cleanPathToWorkingDir,
+      "public/locales/ja/common.json"
+    );
+    const copyJa = fs.existsSync(sourceJaJson);
+    if (copyJa) {
+      if (
+        !fs.existsSync(path.join(cleanPathToWorkingDir, "public/locales/ja"))
+      ) {
+        fs.mkdirSync(path.join(cleanPathToWorkingDir, "public/locales/ja"), {
+          recursive: true,
+        });
+      }
+      console.log(
+        `>> [4] 일본어 업데이트 완료  [${sourceJaJson}] -> [${destJaJson}]`
+      );
+      fs.copyFileSync(sourceJaJson, destJaJson);
+    } else {
+      console.log(`>> [4] 일본어 스킵. 파일 없음 : [${sourceJaJson}]`);
+    }
+
+    console.log(
+      "===============================[ COPY STRING FINISHED ]=============================="
+    );
+  }
+
+  theFunction();
+};
---
+++ dubutils.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const { OptionBuilder } = require("./optionbuilder.js");
+const inputArgs = process.argv.slice(2).map((arg) => arg.toLowerCase());
+
+const possibleCommands = ["buildenv", "copystring"];
+const command = inputArgs.at(0);
+
+// if (inputArgs.at(0) === "help") {
+//   const options = new OptionBuilder(inputArgs);
+//   const {help} = require("./help.js");
+//   help(options);
+// }
+
+const options = new OptionBuilder(inputArgs.slice(1));
+
+const { help } = require("./help.js");
+
+switch (command) {
+  case "buildenv":
+    const { buildenv } = require("./buildenv.js");
+    console.log({ buildenv });
+    buildenv(options);
+    break;
+  case "copystring":
+    const { copystring } = require("./copystring.js");
+    copystring(options);
+    break;
+  case "help":
+    help(options);
+    break;
+  default:
+    console.log(
+      `비어있거나 지원하지 않는 커맨드입니다. 입력한 커맨드 : [${inputArgs.join(", ")}]`
+    );
+    help(options);
+}
