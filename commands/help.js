49e652e9b7c131d5528c2f9b02666d712a3eb826
Stan <stan@2meu.me>
2024-01-05 12:24:20 +0900

    이미지 최적화 추가

--- commands/help.js
+++ commands/help.js
@@ -10,6 +10,10 @@ exports.help = function (options) {
      기본값 : ${options.copystring().searchThrough.join(", ")} 에서 ${options.copystring().searchFor}를 찾음
 
    - parseurl : 로컬 작업폴더의 pages 하위에 있는 파일들을 파싱하여 url을 추출
+
+   - image : 이미지 파일 최적화
+     기본값 : "${options.options.image.src}" 폴더 내의 [${options.options.image.extensions}]이미지를 최적화하여 "${options.options.image.dst}/${options.options.image.src}"에 저장
+      
  
   * 사용 예시
    - dubutils buildenv
